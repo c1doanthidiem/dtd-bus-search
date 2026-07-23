c53f016d3fad4d660965344c9ad4c1d8befbe5f6
c1doanthidiem <c1doanthidiem-ntl@hanoiedu.vn>
2026-07-23 10:39:49 +0700

    Refactor bus information display in app.js
    Updated the HTML structure for displaying bus information, including changes to the return values for 'diemVe' and 'giaoVien'.

--- js/app.js
+++ js/app.js
@@ -11,15 +11,15 @@ input.oninput = () => {
     }
     const list = searchBus(k);
     count.innerHTML = 'Tìm thấy ' + list.length + ' kết quả';
-    result.innerHTML = list.map(i => `
-        <div class="card">
-            <b>${i.tuyen}</b><br>
-            <strong>📍 Điểm đi:</strong> ${i.diemDon}<br>
-            <strong>🕒 Giờ đi:</strong> ${i.gioDi}<br>
-            <strong>📍 Điểm về:</strong> Đối diện điểm đi<br>
-            <strong>🕒 Giờ về:</strong> ${i.gioVe}<br>
-            <strong class="teacher">👩‍🏫 Giáo viên:</strong> ${i.giaoVien}<br>
-            <strong class="contact">📞 Liên hệ:</strong> ${i.dienThoai}
-        </div>
-    `).join('');
+result.innerHTML = list.map(i => `
+    <div class="card">
+        <b>${i.tuyen}</b><br>
+        <strong>📍 Điểm đi:</strong> ${i.diemDon}<br>
+        <strong>🕒 Giờ đi:</strong> ${i.gioDi}<br>
+        <strong>📍 Điểm về:</strong> ${i.diemVe}<br>
+        <strong>🕒 Giờ về:</strong> ${i.gioVe}<br>
+        <strong class="teacher">👩‍🏫 Giám sát xe:</strong> ${i.giaoVien}<br>
+        <strong class="contact">📞 Liên hệ:</strong> ${i.dienThoai}
+    </div>
+`).join('');
 };
