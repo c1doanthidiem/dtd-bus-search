ca63a249f297acd0bbbbc0022147d6786c67e124
c1doanthidiem <c1doanthidiem-ntl@hanoiedu.vn>
2026-07-23 10:05:57 +0700

    changes 2

--- js/app.js
+++ js/app.js
@@ -15,17 +15,13 @@ input.oninput = () => {
 
   count.innerHTML = `Tìm thấy ${list.length} kết quả`;
 
-  result.innerHTML = list
-    .map(
-      (i) => `
-      <div class="card">
-        <b>${i.tuyen}</b><br>
-        ${i.diemDon}<br>
-        ${i.gioDi}<br>
-        <strong>${i.giaoVien}</strong><br>
-        📞 ${i.dienThoai.replace(/(\d{4})(\d{3})(\d{3})/, "$1 $2 $3")}
-      </div>
-    `
-    )
-    .join("");
-};
+ result.innerHTML = list.map(i => `
+<div class="card">
+    <b>${i.tuyen}</b><br>
+
+    📍 <strong>Điểm đón:</strong> ${i.diemDon || "Chưa cập nhật"}<br>
+    🕒 <strong>Giờ đón:</strong> ${i.gioDi || "Chưa cập nhật"}<br>
+    👩‍🏫 <strong>Giáo viên:</strong> ${i.giaoVien}<br>
+    📞 <strong>Liên hệ:</strong> ${(i.dienThoai || "").replace(/(\d{4})(\d{3})(\d{3})/, "$1 $2 $3")}
+</div>
+`).join('');
